5011f62cbdfe777c318e527514e359e9e341f5bd
Nikhil <nikhil@tartanhq.com>
2022-06-26 10:18:18 +0530

    applayout

--- src/utils/About/About_data.ts
+++ src/utils/About/About_data.ts
@@ -6,6 +6,6 @@ export const about_info = {
 
 export const about_founder = {
     name: 'Shubham Gupta',
-    text: 'Shubham Gupta is a civil engineer having experience in variety of structures including buildings and road works. He was alumnus of Navodaya vidyalaya, NIT Jalandhar and IIT Delhi.<br/>And he passionate to spread Learning, Knowledge and Awareness among people through this platform which  helps all in bridge up the gaps of your knowledge. So, educate yourself in your domain which matter in your life and think of how you can make the world a better place for everyone!',
+    text: 'Shubham Gupta is a civil engineer having experience in variety of structures including buildings and road works. He was alumnus of Navodaya vidyalaya, NIT Jalandhar and IIT Delhi.<br/><br/>And he passionate to spread Learning, Knowledge and Awareness among people through this platform which  helps all in bridge up the gaps of your knowledge.<br/><br/>So, educate yourself in your domain which matter in your life and think of how you can make the world a better place for everyone!',
     img: '/shubham.jpeg'
 }
